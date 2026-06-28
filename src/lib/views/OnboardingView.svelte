afcc5fde02bfe11de48750b3a70b936b331e6671
nekename <63245705+nekename@users.noreply.github.com>
2026-06-28 14:57:02 +0100

    feat: create skeleton onboarding view for joining the first community

---
+++ src/lib/views/OnboardingView.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+	import { t } from "$lib/i18n";
+
+	let repositoryInput: HTMLInputElement;
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center">
+	<h1 class="text-3xl font-semibold">{$t("onboarding_view.welcome")}</h1>
+	<p class="mt-2 mb-4 text-lg">{$t("onboarding_view.join_community")}</p>
+
+	<div class="join">
+		<input
+			bind:this={repositoryInput}
+			type="text"
+			class="input not-user-invalid:input-primary user-invalid:validator join-item w-96"
+			placeholder="https://github.com/author/repo"
+			pattern="^(https?://)(github\.com/)[a-zA-Z0-9_-]+/[a-zA-Z0-9_-]+$"
+		/>
+		<button class="btn btn-primary join-item">
+			{$t("onboarding_view.join")}
+		</button>
+	</div>
+
+	<span class="mt-2 text-sm">
+		{$t("onboarding_view.join_fossim_community.1")}
+		<button
+			onclick={() =>
+				(repositoryInput.value =
+					"https://github.com/nekename/fossim-application")}
+			class="link link-info link-hover"
+		>
+			{$t("onboarding_view.join_fossim_community.2")}
+		</button>
+		{$t("onboarding_view.join_fossim_community.3")}
+	</span>
+</div>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,1 +1,11 @@
-<button class="btn btn-primary">Hello, world!</button>
+<script lang="ts">
+	import OnboardingView from "$lib/views/OnboardingView.svelte";
+
+	import { communities } from "$lib/stores";
+</script>
+
+{#if Object.values(communities.state)
+	.map((v) => v.length)
+	.reduce((a, b) => a + b, 0) === 0}
+	<OnboardingView />
+{/if}
